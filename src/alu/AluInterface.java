b28f0e608aa72fcd81c264b0dd67ea36c44a8689
Huangnli <50835276+Huangnli@users.noreply.github.com>
2019-10-04 10:24:42 -0400

    adicionar alu and e or so com um bit

--- src/alu/AluInterface.java
+++ src/alu/AluInterface.java
@@ -2,8 +2,8 @@ package alu;
 
 public interface AluInterface {
 	
-	char[] Or (char num1,char num2);
-    char[] And (char num1,char num2);
+	char Or (char num1,char num2);
+    char And (char num1,char num2);
     char[] Add (char num1,char num2);
     char[] Sub (char num1,char num2);
     char slt (char num1,char num2);
--- src/alu/OpALU.java
+++ src/alu/OpALU.java
@@ -1,16 +1,27 @@
 package alu;
 
 public class OpALU implements AluInterface {
-	public char[] Or(char num1, char num2) {
+	public char Or(char num1, char num2) {
         // TODO Auto-generated method stub
-		char [] a = null; //Remover
-        return a;
+		if(num1 == 0 && num2 == 0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
     }
 
-    public char[] And(char num1, char num2) {
+    public char And(char num1, char num2) {
         // TODO Auto-generated method stub
-        int result;
-        result = num1 + num2;
+        if(num1 == 1 && num2 == 1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+
+
+        
         
         char [] a = null; //Remover
         return a;
