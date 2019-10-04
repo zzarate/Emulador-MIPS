62dd18e2e74c7e14e0e734e96da9ee415c25e6b5
Huangnli <50835276+Huangnli@users.noreply.github.com>
2019-10-04 11:11:52 -0400

    Terminei de fazer Add

--- src/alu/AluInterface.java
+++ src/alu/AluInterface.java
@@ -4,7 +4,7 @@ public interface AluInterface {
 	
 	char Or (char num1,char num2);
     char And (char num1,char num2);
-    char[] Add (char num1,char num2);
+    char[] Add (char[] num1,char[] num2);
     char[] Sub (char num1,char num2);
     char slt (char num1,char num2);
     char bne (char num1,char num2);
--- src/alu/OpALU.java
+++ src/alu/OpALU.java
@@ -1,5 +1,7 @@
 package alu;
 
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.And;
+
 public class OpALU implements AluInterface {
 
 	public char Or(char num1, char num2) {
@@ -22,10 +24,44 @@ public class OpALU implements AluInterface {
         }
     }
 
-    public char[] Add(char num1, char num2) {
+    public char[] Add(char[] num1, char[] num2) {
         // TODO Auto-generated method stub
-    	char [] a = null; //Remover
-        return a;
+        int carryin = 0;
+        int carryout = 0;
+        char[] result;
+        result = new char[63];
+    	for(int i = 0; i < 64; i++){
+            int and = And(num1[i], num2[i]);
+            int or = Or(num1[i], num2[i]);
+            if(carryin == 0){
+                if(and == 1 && or == 1){
+                    result[i] = 0;
+                    carryout = 1;
+                }
+                else if(and == 0 && or == 0){
+                          result[i] = 0;
+                          carryout = 0; 
+                }
+                else{
+                    result[i] = 1;
+                    carryout = 0;
+                }
+            }
+            else{
+                if(and == 1 && or == 1){
+                    result[i] = 1;
+                    carryout = 1;
+                }
+                else if(and == 0 && or == 0){
+                          result[i] = 1;
+                          carryout = 0; 
+                }
+                else{
+                    result[i] = 0;
+                    carryout = 1;
+                }
+            }
+        }   
     }
 
     public char[] Sub(char num1, char num2) {
