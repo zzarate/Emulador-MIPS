4ab7738cbe5e0736a4742b2c3311d04be903f8e0
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-10-07 18:36:33 -0400

    Alteracao para char

--- src/registradores/HILO.java
+++ src/registradores/HILO.java
@@ -2,19 +2,19 @@ package registradores;
 
 public class HILO {
 	//HILO
-    private int[] HILO = new int [64];
+    private char[] HILO = new char [64];
 
     //Define valor no HILO
-    void setHilo (int [] value_64){
+    void setHilo (char [] value_64){
         for (int i =0; i<64; i++){
             HILO[i] = value_64 [i];
         }
     }
 
     //Devolve valor armazenaddo no LO
-    int [] getLO (){
-        int [] dest_LO;
-        dest_LO = new int [32];
+    char [] getLO (){
+        char [] dest_LO;
+        dest_LO = new char [32];
 
         for (int i = 0; i<32; i++) {
             dest_LO[i]= HILO [i];
@@ -23,9 +23,9 @@ public class HILO {
     }
 
     //Devolve valor armazenaddo no HI
-    int [] getHI (){
-        int [] dest_HI;
-        dest_HI = new int [32];
+    char [] getHI (){
+        char [] dest_HI;
+        dest_HI = new char [32];
         for (int i = 32; i<64; i++) {
             dest_HI[i]= HILO [i];
         }
