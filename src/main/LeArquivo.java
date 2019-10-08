52f584a03e5c71364f808cb285a5358813f838e0
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-10-07 21:06:56 -0400

    Alterado o algoritmo para ler arquivos

--- src/main/LeArquivo.java
+++ src/main/LeArquivo.java
@@ -1,30 +1,29 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class LeArquivo {
 	
 	//Abre o arquivo binario em MIPS
 	void abrirArquivo () throws IOException  {
-		System.out.println("Insira o caminho do arquivo a ser lido: ");
+
+		//Le o arquivo .bin
+		System.out.println("Insira o caminho do arquivo (.bin) a ser lido: ");
 		Scanner scanner = new Scanner (System.in);
 		String localArquivo = scanner.nextLine();
 
-		FileInputStream arquivo = new FileInputStream(localArquivo);
+		File arquivo = new File(localArquivo);
 
-		//Especificando codificação UTF-8 explicitamente
-		InputStreamReader inputStreamReader = new InputStreamReader(arquivo, "UTF-8");
-
-		try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader) ) {
-			String linha;
-			while ((linha = bufferedReader.readLine()) != null) {
-				System.out.println (linha);
-			}
+		try {
+			byte [] conteudo = Files.readAllBytes(arquivo.toPath());
+			System.out.println(new String(conteudo));
 		} catch (Exception e) {
+			System.out.println("Erro ao ler o arquivo! Tente outro arquivo. ");
+			System.out.println("Detalhes do erro: ");
+			System.out.println("");
 			e.printStackTrace();
 		}
 		scanner.close();
