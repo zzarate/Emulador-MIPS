fd708b20f586c9935fa407b34fc7db1a398bd700
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-10-09 15:43:39 -0400

    Firulas

--- src/main/InicioPrograma.java
+++ src/main/InicioPrograma.java
@@ -5,8 +5,14 @@ import java.io.IOException;
 public class InicioPrograma {
     public static void main(String[] args) throws IOException {
         
-        //Teste
         LeArquivo arquivo = new LeArquivo();
         arquivo.abrirArquivo();
+        
+        //Exibe uma mensagem avisando que o programa terminou de ser executado (????)
+        System.out.println(" ___________________________");
+        System.out.println("|                           |");
+        System.out.println("|    Execução finalizada    |");
+        System.out.println("|                           |");
+        System.out.println("|___________________________|");
     }
 }
