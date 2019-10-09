b1bbf973ff14157f04b63e60924cb8a84b180b0c
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-10-09 15:44:01 -0400

    Definicao metodo para decodificacao

--- src/main/Decodifica.java
+++ src/main/Decodifica.java
@@ -2,18 +2,7 @@ package main;
 
 public class Decodifica {
 
-    void decodificaInstruc (char [] instucao){
-        char [] opCode = new char [6];
-        for (int i = 0; i < 6; i++) {
-            opCode [i] = instucao [26+i];
-        }
-
-        for (int i = 0; i < 6; i++) {
-            if (opCode [i] == 0){
-                //Insturção tipo R
-            }
-        }
+    void decodificaInstrucao (String instrucao){
         
-
     }
 }
