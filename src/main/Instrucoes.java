203b3397b477c0b185cdca0263736058205ef041
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-10-10 00:17:22 -0400

    Valores das constantes atualizadas para o valor correto

--- src/main/Instrucoes.java
+++ src/main/Instrucoes.java
@@ -11,68 +11,62 @@ public class Instrucoes {
     //Valores do OpCode
 
     //Tipo R
-    public static final char [] R = { '0','0','0','0','0','0'};
+    public static final char [] R = { '0','0','0','0','0','0'};     //Instruções do tipo R
 
     //Aritmetica
+    public static final char [] addi = { '0','0','1','0','0','0'};  //Add Immediate
+    public static final char [] addiu = { '0','0','1','0','0','1'}; //Add Immediate Unsigned 
     
-
-
-    //Alterar valor do conjunto
+    //Transferencia de dados
+    public static final char [] lw = { '1','0','0','0','1','1'};    //Load Word
+    public static final char [] lh = { '1','0','0','0','0','1'};    //Load Halfword
+    public static final char [] lb = { '1','0','0','0','0','0'};    //Load Byte
+    public static final char [] sw = { '1','0','1','0','1','1'};    //Store Word
+    public static final char [] sh = { '1','0','1','0','0','1'};    //Store Halfword
+    public static final char [] sb = { '1','0','1','0','0','0'};    //Store Byte
+    public static final char [] lui = { '0','0','1','1','1','1'};   //Load Upper Immediate
     
-    public static final char [] addi = { '1','0','0','0','0','0'};
-    public static final char [] addiu = { '1','0','0','0','0','0'};
-    
+    //Logica
+    public static final char [] andi = { '0','0','1','1','0','0'};  //And Immediate 
+    public static final char [] ori = { '0','0','1','1','0','1'};   //Or Immediate 
+    public static final char [] slti = { '0','0','1','0','1','0'};  //Set on Less Than Immediate
 
+    //Desvio condicional
+    public static final char [] beq = { '0','0','0','1','0','0'};   //Branch on equal
+    public static final char [] bne = { '0','0','0','1','0','1'};   //Branch on not equal
 
-    public static final char [] lw = { '1','0','0','0','0','0'};
-    public static final char [] lh = { '1','0','0','0','0','0'};
-    public static final char [] lb = { '1','0','0','0','0','0'};
-    public static final char [] sw = { '1','0','0','0','0','0'};
-    public static final char [] sh = { '1','0','0','0','0','0'};
-    public static final char [] sb = { '1','0','0','0','0','0'};
-    public static final char [] lui = { '1','0','0','0','0','0'};
-    
+    //Desvio incondicional - Tipo J
+    public static final char [] j = { '0','0','0','0','1','0'};     //Jump
+    public static final char [] jal = { '0','0','0','0','1','1'};   //Jump and link
 
-
-    
-    public static final char [] andi = { '1','0','0','0','0','0'};
-    
-    public static final char [] ori = { '1','0','0','0','0','0'};
-    public static final char [] slt = { '1','0','0','0','0','0'};
-    public static final char [] slti = { '1','0','0','0','0','0'};
-
-
-    
-
-
-    public static final char [] beq = { '1','0','0','0','0','0'};
-    public static final char [] bne = { '1','0','0','0','0','0'};
-
-    public static final char [] j = { '1','0','0','0','0','0'};
-    
-    public static final char [] jal = { '1','0','0','0','0','0'};
+    //---------------------------------------------------------------------------------------//
 
     //Valor do funct para instruções do tipo R
+    //Aritmetica - funct
+    public static final char [] add = { '1','0','0','0','0','0'};   //Add
+    public static final char [] sub = { '1','0','0','0','1','0'};   //Sub
+    public static final char [] mult = { '0','1','1','0','0','0'};  //Multiply
+    public static final char [] div = { '0','1','1','0','1','0'};   //Divide
 
-    public static final char [] add = { '1','0','0','0','0','0'};
-    public static final char [] sub = { '1','0','0','0','0','0'};
-    public static final char [] div = { '1','0','0','0','0','0'};
-    public static final char [] mult = { '1','0','0','0','0','0'};
+    //Transferencia de dados - funct
+    public static final char [] mfhi = { '0','1','0','0','0','0'};  //Move from high
+    public static final char [] mflo = { '0','1','0','0','1','0'};  //Move from low
+    
+    //Logica - funct
+    public static final char [] and = { '1','0','0','1','0','0'};   //And
+    public static final char [] or = { '1','0','0','1','0','1'};    //Or
+    public static final char [] slt = { '1','0','1','0','1','0'};   //Set on Less Than
 
-    public static final char [] mfhi = { '1','0','0','0','0','0'};
-    public static final char [] mflo = { '1','0','0','0','0','0'};
+    //Deslocamento de bits- funct
+    public static final char [] sll = { '0','0','0','0','0','0'};   //Shift left logical
+    public static final char [] srl = { '0','0','0','0','1','0'};   //Shift right logical
+    public static final char [] sra = { '0','0','0','0','1','1'};   //Shift right arithmetic
 
-    public static final char [] sll = { '1','0','0','0','0','0'};
-    public static final char [] srl = { '1','0','0','0','0','0'};
-    public static final char [] sra = { '1','0','0','0','0','0'};
+    //Desvio incondiciona- - funct
+    public static final char [] jr = { '0','0','1','0','0','0'};    //Jump register
 
-    public static final char [] and = { '1','0','0','0','0','0'};
-    public static final char [] or = { '1','0','0','0','0','0'};
-
-    public static final char [] jr = { '1','0','0','0','0','0'};
-
-
-    public static final char [] syscall = { '0','0','1','1','0','0'};
+    //Syscall
+    public static final char [] syscall = { '0','0','1','1','0','0'};  //--Valor correto* 
 
 
 
