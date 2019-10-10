c895ab05908dae683defd4fa73bb22a223aa8572
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-10-09 23:32:17 -0400

    Adicionado corpo do metodo

--- src/main/Decodifica.java
+++ src/main/Decodifica.java
@@ -1,8 +1,19 @@
 package main;
 
 public class Decodifica {
+    char opCode [];
+    char instrucao [];
 
     void decodificaInstrucao (String instrucao){
+        opCode = new char [6];
+        this.instrucao = new char [32];
+        this.instrucao = instrucao.toCharArray();
         
+        for (int i = 0; i <6; i++) {
+            opCode [i] = this.instrucao[i];
+        }
+        System.out.println ("Instrucao: ");
+        System.out.println (opCode);
+        System.out.println ("");
     }
 }
