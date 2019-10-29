13b01d5a1bccd4f18f180eb97989b1d998a0a5ce
Huangnli <50835276+Huangnli@users.noreply.github.com>
2019-10-29 10:17:17 -0400

    Update Syscall.java

--- src/instrucoes/Syscall.java
+++ src/instrucoes/Syscall.java
@@ -7,25 +7,25 @@ class Syscall {
     void verifica (OperacoesRegistradores opReg){
 
         if (Integer.parseInt(new String (opReg.getValorReg(2))) == 1 ) {
-            
+            //print integer ($a0 = integer to print)
         }
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
-            
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 4 ) {
+            //print string ($a0 = address of null-terminated string to print)
         }
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
-            
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 5 ) {
+            //read integer ($v0 contains integer read)
         } 
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
-            
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 8 ) {
+            //read string
         }
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
-            
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 10 ) {
+            //exit (terminate execution)
         }
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
-            
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 11 ) {
+            //print character ($a0 = character to print,  	See note below table)
         }
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
-            
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 12 ) {
+            //read character ($v0 contains character read)
         }
         if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
             
