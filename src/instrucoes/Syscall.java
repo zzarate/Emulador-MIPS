81b45d7489db36c019919c4639e92d6345c75bad
Huangnli <50835276+Huangnli@users.noreply.github.com>
2019-10-29 10:06:07 -0400

    Update Syscall.java

--- src/instrucoes/Syscall.java
+++ src/instrucoes/Syscall.java
@@ -6,7 +6,7 @@ class Syscall {
     
     void verifica (OperacoesRegistradores opReg){
 
-        if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
+        if (Integer.parseInt(new String (opReg.getValorReg(2))) == 1 ) {
             
         }
         if (Integer.parseInt(new String (opReg.getValorReg(2))) == x ) {
